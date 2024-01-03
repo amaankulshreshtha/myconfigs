7972dc1eaebffb83327a49b6377f68cfea6f7a5c
Amaan Kulshreshtha <amaan.kulshreshtha@accenture.com>
2024-01-03 12:00:31 +0400

    change zsh-ssh-completion path in plugins file

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -1,3 +1,3 @@
 [[ -f ~/.zsh/plugins/zsh-z/zsh-z.plugin.zsh ]] && . ~/.zsh/plugins/zsh-z/zsh-z.plugin.zsh
-[[ -f ~/.zsh/plugins/zsh-ssh/zsh-ssh.plugin.zsh ]] && . ~/.zsh/plugins/zsh-ssh/zsh-ssh.plugin.zsh
+[[ -f ~/.zsh/plugins/zsh-ssh-completion/zsh-ssh.plugin.zsh ]] && . ~/.zsh/plugins/zsh-ssh-completion/zsh-ssh.plugin.zsh
 [[ -f ~/.zsh/plugins/zsh-nvm/zsh-nvm.plugin.zsh ]] && . ~/.zsh/plugins/zsh-nvm/zsh-nvm.plugin.zsh
